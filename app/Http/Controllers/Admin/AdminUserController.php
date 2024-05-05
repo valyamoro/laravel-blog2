8d5b3116813043bfed1ab6f406a8171704bdcdf4
valyamoro <moralek228@gmail.com>
2024-05-05 14:00:35 +0300

    dev 3 feat (Контроллер admin user)

---
+++ app/Http/Controllers/Admin/AdminUserController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\AdminUserRequest;
+use App\Models\AdminUser;
+
+class AdminUserController extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(AdminUserRequest $request)
+    {
+        //
+    }
+
+    public function show(AdminUser $adminUser)
+    {
+        //
+    }
+
+    public function edit(AdminUser $adminUser)
+    {
+        //
+    }
+
+    public function update(AdminUserRequest $request, AdminUser $adminUser)
+    {
+        //
+    }
+
+    public function destroy(AdminUser $adminUser)
+    {
+        //
+    }
+
+}
