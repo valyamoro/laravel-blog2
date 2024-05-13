22b429518b72918acb18aafaa54da25cb4b1333c
valyamoro <moralek228@gmail.com>
2024-05-13 05:27:15 +0300

    dev 8 feat (Связи модели article)

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 /**
  * Class Category
@@ -47,6 +47,16 @@ class Article extends BaseModel
                 'source' => 'title',
             ],
         ];
+    }
+
+    public function adminUser(): BelongsTo
+    {
+        return $this->belongsTo(AdminUser::class, 'user_id');
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
     }
 
 }
