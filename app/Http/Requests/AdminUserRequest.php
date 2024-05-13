38d18339828b6cbd60596f45d06930f9b7c4bb44
valyamoro <moralek228@gmail.com>
2024-05-13 03:11:09 +0300

    dev 6 fix (Реквест admin user)

--- app/Http/Requests/AdminUserRequest.php
+++ app/Http/Requests/AdminUserRequest.php
@@ -13,23 +13,11 @@ class AdminUserRequest extends FormRequest
 
     public function rules(): array
     {
-        $id = $this->admin_user->id ?? null;
-
         return [
             'username' => 'required|string|min:2|max:255|regex:/[A-Za-zА-ЯЁа-яё]+/',
-            'email' => 'required|string|max:255|email|unique:admin_users,email,' . $id,
-            'password' => $this->isRequired() . 'string|min:5|max:255|regex:/^[A-Za-zА-ЯЁа-яё0-9]+$/' . $this->isConfirmed(),
+            'email' => 'required|string|max:255|email|unique:admin_users,email,' . $this->admin_user?->id,
+            'password' => ($this->admin_user?->password ? 'nullable' : 'required') . '|string|min:5|max:255|regex:/^[A-Za-zА-ЯЁа-яё0-9]+$/|confirmed',
         ];
-    }
-
-    private function isRequired(): string
-    {
-        return !isset($this->admin_user) ? 'required|' : 'nullable|';
-    }
-
-    private function isConfirmed(): string
-    {
-        return !isset($this->admin_user) ? '|confirmed' : '';
     }
 
 }
