bf01c3b2c78745ab6f279410a49dd6da56983450
valyamoro <moralek228@gmail.com>
2024-05-22 06:56:32 +0300

    dev 10 feat (Тесты comment)

---
+++ tests/Feature/Admin/Comment/CommentControllerTest.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace Admin\Comment;
+
+use App\Models\AdminUser;
+use App\Models\Article;
+use App\Models\Comment;
+use App\Services\AdminUsers\AdminUserRepository;
+use App\Services\Comments\CommentRepository;
+use App\Services\Comments\CommentService;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\Request;
+use Tests\TestCase;
+
+class CommentControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    private CommentService $commentService;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $commentRepository = new CommentRepository();
+        $adminUserRepository = new AdminUserRepository();
+        $this->commentService = new CommentService($commentRepository, $adminUserRepository);
+
+        $this->actingAs(AdminUser::factory()->create(), 'admin');
+
+        $this->withoutMiddleware(\App\Http\Middleware\VerifyCsrfToken::class);
+    }
+
+    public function testGetViewCommentsIndex(): void
+    {
+        $perPage = 5;
+        $title = 'Комментарии';
+        $request = new Request();
+
+        $response = $this->get(route('comments.index'));
+        $comments = $this->commentService->getAllWithPagination($request, $perPage);
+
+        $response->assertSuccessful();
+        $response->assertViewIs('admin.comments.index');
+        $response->assertViewHas([
+            'title' => $title,
+            'paginator' => $comments,
+        ]);
+    }
+
+    public function testCommentCreate(): void
+    {
+        Article::factory()->create(['id' => 1]);
+        $commentData = [
+            'comment' => 'test comment data',
+            'article_id' => 1,
+        ];
+
+        $response = $this->post(route('comments.store'), $commentData);
+
+        $this->assertDatabaseCount(Comment::class, 1);
+        $this->assertDatabaseHas(Comment::class, $commentData);
+        $response->assertSessionHas('success', 'Ваш комментарий был успешно принят, но будет опубликован после проверки модератором.');
+    }
+
+    public function testGetViewCommentsShow(): void
+    {
+        Article::factory()->create(['id' => 1]);
+        $commentData = [
+            'id' => 1,
+            'comment' => 'Test data comment',
+            'article_id' => 1,
+        ];
+        $comment = Comment::factory()->create($commentData);
+        $title = 'Комментарий: #1';
+
+        $response = $this->get(route('comments.show', $comment));
+
+        $response->assertSuccessful();
+        $response->assertViewIs('admin.comments.show');
+        $response->assertViewHas([
+            'item' => $comment,
+            'title' => $title,
+        ]);
+    }
+
+    public function testCommentUpdate(): void
+    {
+        Article::factory()->create(['id' => 1]);
+        $commentData = [
+            'comment' => 'Test data comment',
+            'article_id' => 1,
+        ];
+        $comment = Comment::factory()->create($commentData);
+
+        $response = $this->put(route('comments.update', $comment), $commentData);
+
+        $this->assertDatabaseCount(Comment::class, 1);
+        $this->assertDatabaseHas(Comment::class, $commentData);
+        $response->assertSessionHas('success', 'Успешно сохранено.');
+        $response->assertRedirect(route('comments.index'));
+    }
+
+    public function testCommentDestroy(): void
+    {
+        Article::factory()->create(['id' => 1]);
+        $commentData = [
+            'comment' => 'Test data comment',
+            'article_id' => 1,
+        ];
+        $comment = Comment::factory()->create($commentData);
+
+        $response = $this->delete(route('comments.destroy', $comment));
+
+        $this->assertDatabaseCount(Comment::class, 0);
+        $this->assertDatabaseMissing(Comment::class, $commentData);
+        $response->assertSessionHas('success', 'Успешно удалено.');
+        $response->assertRedirect(route('comments.index'));
+    }
+
+}
---
+++ tests/Feature/Admin/Comment/CommentRequestTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Admin\Comment;
+
+use App\Models\AdminUser;
+use App\Models\Article;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Str;
+use Tests\TestCase;
+
+class CommentRequestTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->actingAs(AdminUser::factory()->create(), 'admin');
+    }
+
+    public function testMoreThanFields(): void
+    {
+        $invalidRequestData = [
+            'comment' => Str::repeat('n', 256),
+        ];
+        $fieldErrorMessage = [
+            'comment' => 'Количество символов в значении поля comment не может превышать 255.',
+        ];
+
+        $this->post(route('comments.store', $invalidRequestData))
+            ->assertInvalid($fieldErrorMessage);
+
+        $this->assertTrue(session()->hasOldInput('comment'));
+    }
+
+    public function testLessThanFields(): void
+    {
+        $invalidRequestData = [
+            'comment' => 'nnnn',
+        ];
+        $fieldErrorMessage = [
+            'comment' => 'Количество символов в поле comment должно быть не меньше 5.',
+        ];
+
+        $this->post(route('comments.store', $invalidRequestData))
+            ->assertInvalid($fieldErrorMessage);
+
+        $this->assertTrue(session()->hasOldInput('comment'));
+    }
+
+    public function testCategoryValidateSuccess(): void
+    {
+        Article::factory()->create(['id' => 1]);
+        $validRequestData = [
+            'article_id' => 1,
+            'comment' => 'Test data comment',
+        ];
+
+        $response = $this->post(route('comments.store'), $validRequestData);
+
+        $response->assertSessionHasNoErrors();
+        $this->assertDatabaseCount(Article::class,  1);
+    }
+
+}
