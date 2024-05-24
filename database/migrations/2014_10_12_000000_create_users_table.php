e5e0222aaba612e6a6c6f93513fdfb3203cd9afb
valyamoro <moralek228@gmail.com>
2024-05-24 17:50:42 +0300

    dev 11 feat (Модель user)

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,27 +6,21 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('username')->comment('Имя');
+            $table->string('email')->unique()->comment('Почта');
+            $table->string('password')->comment('Пароль');
+            $table->boolean('is_banned')->default(false)->comment('Бан');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('users');
     }
+
 };
